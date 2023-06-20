ee5f3414236e0f1b29bdf5e1d730684a7bdce5ea
sammed mangave <105945906+samycolen@users.noreply.github.com>
2023-06-20 15:44:40 +0530

    Create Basic_NLP_Model.py
    NLP_Model

---
+++ Basic_NLP_Model.py
@@ -0,0 +1,3 @@
+In this project we created a simple NLP model which removes the html tags and provided stopwords from the given set of sentences
+
+the model tokenize the corpus words using the Tokenizer from karas library
